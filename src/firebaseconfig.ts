a5450713573a7bdc170587ba4bba32ac52953eb5
filipecheverrya <filipecheverrya@gmail.com>
2025-06-14 23:54:33 -0300

    Delete src/firebaseconfig.ts

--- src/firebaseconfig.ts
+++
@@ -1,9 +0,0 @@
-export const config = {
-  apiKey: "AIzaSyAjEO_UMM2NjsNehYs2lJMlwLNcs8ayqlU",
-  authDomain: "my-rated-movies.firebaseapp.com",
-  projectId: "my-rated-movies",
-  storageBucket: "my-rated-movies.firebasestorage.app",
-  messagingSenderId: "329230021302",
-  appId: "1:329230021302:web:984b01264e7970ed9d2c17",
-  measurementId: "G-45VE6R25JV"
-}
